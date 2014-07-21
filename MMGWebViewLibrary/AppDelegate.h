17cd105a886c37b8ad693e3dc7ec148b5bfc1d69
Mohamed-Mitwaly <iMeto@Mohameds-MacBook-Pro.local>
2014-07-21 11:18:25 +0200

    Initial Commit

---
+++ MMGWebViewLibrary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MMGWebViewLibrary
+//
+//  Created by Mohamed Mitwaly on 7/21/14.
+//  Copyright (c) 2014 MMG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MMGWebViewLibrary/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MMGWebViewLibrary
+//
+//  Created by Mohamed Mitwaly on 7/21/14.
+//  Copyright (c) 2014 MMG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
